8b8c17176b177e777c08fcd69de7c119212d659c
Rompetr78 <rompetr@yahoo.com>
2024-06-19 11:53:23 +0800

    добавлена функия получения типа переменной getType

--- lecture_1.java
+++ lecture_1.java
@@ -1,7 +1,26 @@
-public class program {
+public class lecture_1 {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        // System.out.println("Hello World!");
+        char ch = 123;
+        System.out.println(ch);
+        System.out.println(getType(ch));
+        var d = 12.45;
+        System.out.println(d);
+        System.out.println(getType(d));
+        int i = 123;
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        String s = "qwer";
+        System.out.println(s.charAt(1));
+        int a = 24;
+        a = --a - a--;
+        System.out.println(a);
+        boolean f = 123 > 234;
+        System.out.println(f);
 
     }
-    
+    static String getType(Object obj){
+        return obj.getClass().getSimpleName();
+    }
 }
+

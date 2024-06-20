3c6660a222e05cde6a56a7d09ab7b9478e2fd81c
Rompetr78 <rompetr@yahoo.com>
2024-06-21 00:04:20 +0800

    added Task0, 1, 2 in seminar_2.java

--- seminar_1.java
+++ seminar_1.java
@@ -2,7 +2,7 @@
 Задание №0
 Вывести в консоль “Hello world!”.
 Вывести в консоль системные дату и время.
-*/
+
 
 import java.sql.Date; // для класса Date (устаревший)
 import java.time.LocalDateTime; // для класса Calendar (устаревший)
@@ -15,5 +15,68 @@ public class seminar_1 {
         //Date
         //Calendar
         System.out.println(LocalDateTime.now());
+        int[] arr = {23, 34, 45};
+
+        for (int elem : arr) {
+           System.out.print (elem + " ");
+        }
+        System.out.println();
     }
 }
+*/
+
+/*
+ * Задание №1
+📌 Написать программу, которая запросит пользователя ввести
+<Имя> в консоли.
+📌 Получит введенную строку и выведет в консоль сообщение
+“Привет, <Имя>!”
+ * 
+ 
+import java.util.Scanner;
+
+public class seminar_1 {
+
+    public static void main(String[] args){
+        // первый вариант
+        // System.out.println("Введите имя");
+        // String name = System.console().readLine();
+        // System.out.println("Привет, " + name + "!");
+
+        // второй вариант
+        Scanner inputScanner = new Scanner(System.in);
+        System.out.println("Введите имя");
+        String name1 = inputScanner.nextLine();
+        System.out.printf("Привет, %s !", name1);
+        System.out.println(" ");   
+        inputScanner.close();
+
+    }
+}
+*/
+
+/*
+ * Задание №2
+Дан массив двоичных чисел, например [1,1,0,1,1,1], вывести
+максимальное количество подряд идущих 1.
+ */
+
+ public class seminar_1 {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 0, 1, 1, 1};
+        int count = 0;
+        int maxCount = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+                if (count > maxCount) {
+                    maxCount = count;
+                }
+            } else {
+                 count = 0;
+                }
+            
+        }
+        System.out.println("Максимальное количество подряд идущих 1: " + maxCount);
+    }
+ }

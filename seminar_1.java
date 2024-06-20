26e0ee36751595c84df9cb9eb9edf3ac67698a23
Rompetr78 <rompetr@yahoo.com>
2024-06-20 12:46:06 +0800

    added task_0 in seminar_1.java

--- seminar_1.java
+++ seminar_1.java
@@ -1,3 +1,19 @@
+/*
+Задание №0
+Вывести в консоль “Hello world!”.
+Вывести в консоль системные дату и время.
+*/
+
+import java.sql.Date; // для класса Date (устаревший)
+import java.time.LocalDateTime; // для класса Calendar (устаревший)
+import java.util.Calendar;
+
 public class seminar_1 {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        //Date
+        //Calendar
+        System.out.println(LocalDateTime.now());
+    }
 }

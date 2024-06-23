74ac67d64c60d226f20512c2b0fd481af0047441
Rompetr78 <rompetr@yahoo.com>
2024-06-23 12:59:27 +0800

    added the Task 4 in seminar_2.java

--- seminar_2.java
+++ seminar_2.java
@@ -119,7 +119,7 @@ public class seminar_2 {
         return str.equals(sb.toString());
     }
 }
-*/
+
 // решение в группе
 
 public class seminar_2 {
@@ -141,3 +141,51 @@ public class seminar_2 {
     }
 
 }
+*/
+
+/*
+Задание №4
+Напишите метод, который составит строку, состоящую из 100
+повторений слова TEST и метод, который запишет эту строку в
+простой текстовый файл, обработайте исключения.
+
+
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class seminar_2 {
+
+    public static void main(String[] args) {
+        String str = "TEST";
+        int n = 100;
+        String newStr = stringMaker(str, n);
+        String fileName = "text.txt";
+        writeToFile(newStr, fileName);
+    }
+
+    public static String stringMaker(String str, int n) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(str).append(" ");
+        } 
+        
+        return sb.toString();
+    }
+
+    public static void writeToFile(String str, String fileName) {
+        try (FileWriter fw = new FileWriter(fileName, true)) {
+            fw.write(str);
+            fw.write(System.lineSeparator());
+            System.out.println("Written is success");
+        } catch (IOException e) {
+            System.err.println("Ошибка записи в файл");
+        }  
+    }
+
+}
+*/
+
+/*
+
+*/

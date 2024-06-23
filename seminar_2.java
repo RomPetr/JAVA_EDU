768507b6c39491690f1c2be29f1a9e5629613ce2
Rompetr78 <rompetr@yahoo.com>
2024-06-23 14:08:16 +0800

    add logger.setUseParentHandlers(false) in seminar_2.java

--- seminar_2.java
+++ seminar_2.java
@@ -187,5 +187,61 @@ public class seminar_2 {
 */
 
 /*
-
+Задание №5
+1. Напишите метод, который вернет содержимое текущей папки в виде
+массива строк.
+2. Напишите метод, который запишет массив, возвращенный предыдущим
+методом в файл.
+3. Обработайте ошибки с помощью try-catch конструкции. В случае
+возникновения исключения, оно должно записаться в лог-файл
 */
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class seminar_2 {
+    private static Logger logger = Logger.getLogger(seminar_2.class.getName());
+    public static void main(String[] args) {
+        congfigLogger();  
+        String fileName = "FileList.txt";
+        String[] contentFolder = getFileList(System.getProperty("user.dir"));
+        writeArrayToFile(contentFolder, fileName);
+    }
+
+    static String[] getFileList(String folderName) {
+        File currentFolder = new File(folderName);
+        return currentFolder.list();
+    }
+
+    static void writeArrayToFile(String[] arr, String fileName) {
+        try (FileWriter fw = new FileWriter(fileName)) {
+            for(String elem : arr) {
+                fw.write(elem);
+                //fw.write("\n");
+                fw.write(System.lineSeparator());
+            }
+            System.out.println("Written is success");
+            logger.info("Данные успешно записаны");
+            } catch (IOException ex) {
+            System.err.println(ex.getLocalizedMessage());
+        }
+    }
+
+    static void congfigLogger() {
+       try {
+        //logger.setUseParentHandlers(false);
+        FileHandler fh = new FileHandler("log.txt", true);
+        logger.addHandler(fh);
+        SimpleFormatter sf = new SimpleFormatter();
+        fh.setFormatter(sf);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}

48b7bad287a17a1095eff4ca7fc34af34741989e
Roman <rompetr@yahoo.com>
2024-07-10 22:59:25 +0800

    added Task 3 in seminar_3.java

--- seminar_3.java
+++ seminar_3.java
@@ -1,6 +1,6 @@
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -167,3 +167,28 @@ public class seminar_3 {
  Пройти по списку, найти и удалить целые числа. 
 */
 
+public class seminar_3 {
+    public static void main(String[] args) {
+        List<String> lst = new ArrayList<>(Arrays.asList("One", "Two", "three", "1", "2", "Hello", "-10", "кто тут?"));
+        deleteInt(lst);
+        System.out.println(lst);
+    }
+
+    private static boolean isInt(String str) {
+        try {
+            Integer.parseInt(str);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    private static void deleteInt (List<String> lst) {
+        for (int i = 0; i < lst.size(); i++) {
+            if(isInt(lst.get(i))) {
+                lst.remove(i);
+                i--;
+            }
+        }
+    }
+}
